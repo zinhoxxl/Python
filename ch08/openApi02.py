445caae51bf97986b17c4641e476a91f54200fda
zinhoxxl <zinhoxxl@gmail.com>
2022-01-04 10:32:08 +0900

    0104

--- ch08/openApi02.py
+++ ch08/openApi02.py
@@ -7,3 +7,28 @@
 # 네이버 검색 API예제는 블로그를 비롯 전문자료까지 호출방법이 동일하므로 blog검색만 대표로 예제
 # 네이버 검색 Open API 예제 - 블로그 검색
 
+import requests
+import pprint
+
+headers = {
+    'X-Naver-Client-Id' : 'D6E8zfH8Ief1IzvShb57' ,
+    'X-Naver-Client-Secret' : '4IbYPU7R6F' ,
+}
+
+payLoad = {
+    'query' : '스프링',
+    'display' : 100,
+}
+
+url = 'https://openapi.naver.com/v1/search/blog' # json 결과
+
+res = requests.get(url, headers=headers, params=payLoad)
+
+print('request sended...')
+#응답이 json 형태로 오기로 되어 있음
+print(res.json())
+
+# json Web Viewer -> jsonviewer.stack.hu(3번째 결과)
+result = res.json()['items'][5]['title']
+print('result 5번째 title만 가져옴')
+print(result)
